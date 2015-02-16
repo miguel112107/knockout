15f46f259ae46ca45eb6882d39fa88bca0583237
Miguel <maflores17@miners.utep.edu>
2015-02-16 15:32:15 -0700

    Demo Started

---
+++ DemoKO/WebApplication1/Scripts/VM_Field.js
@@ -0,0 +1,48 @@
+﻿// Class to represent a row in the seat reservations grid
+function spotReservation(defaultPlayer) {
+    var self = this;
+    self.name = name;
+    self.playerOnField = ko.observable(defaultPlayer);
+  
+        
+}
+
+// Overall viewmodel for this screen, along with initial state
+function ReservationsViewModel() {
+    var self = this;
+
+    // Non-editable catalog data - would come from the server
+    self.availablePlayers = [
+        { playerName: "I. Casillas"},
+        { playerName: "D. Carvajal"},
+        { playerName: "S. Ramos"},
+        { playerName: "Pepe"},
+        { playerName: "R. Varane"},
+        { playerName: "Marcelo"},
+        { playerName: "F. Coentrao"},
+        { playerName: "T. Kroos"},
+        { playerName: "L. Modric"},
+        { playerName: "L. Silva"},
+        { playerName: "C. Ronaldo"},
+        { playerName: "Jese"},
+        { playerName: "G. Bale"},
+        { playerName: "J. Rodriguez"},
+        { playerName: "K. Benzema"}
+    ];
+
+    // Editable data
+    self.availablePosition = ko.observableArray([
+        new spotReservation(self.availablePlayers[0]),
+    ]);
+    //self.finalRoster = ko.observableArray([
+    //    new 
+   // ])
+    self.addSeat = function () {
+        self.availablePosition.push(new spotReservation(self.availablePlayers[0]));
+    }
+
+    self.Keeper = self.availablePlayers[0].valueOf();
+    
+}
+    
+ko.applyBindings(new ReservationsViewModel());
